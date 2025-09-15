317ff340716b974328a86bcf77720a395f0e264d
Константин Завьялов <Tyuiu.ZavyalovKA@outlook.com>
2025-09-15 23:21:38 +0500

    Task1

--- Tyuiu.ZavyalovKA.Sprint0.Task1.V0/Program.cs
+++ Tyuiu.ZavyalovKA.Sprint0.Task1.V0/Program.cs
@@ -1,11 +1,12 @@
 ﻿internal class Program
 {
-    private static void Main(string[] args)
+    static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
         string name = "Константин";
         int age = 18;
         Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
         Console.ReadKey();
+
     }
 }
