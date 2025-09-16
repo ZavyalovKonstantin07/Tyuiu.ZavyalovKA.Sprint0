ff99b0174b88878284766cbdf0d7e4582cd58c00
Константин Завьялов <Tyuiu.ZavyalovKA@outlook.com>
2025-09-16 13:03:03 +0500

    Task2

---
+++ Tyuiu.ZavyalovKA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Tyuiu.ZavyalovKA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.ZavyalovKA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Константин";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет..., Константин", res);
+        }
+    }
+}
---
+++ Tyuiu.ZavyalovKA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.ZavyalovKA.Sprint0.Task2.V0.Lib;
+Console.WriteLine(DataService.GetMessage("Константин"));
+Console.ReadKey();
+Console.ReadKey();
