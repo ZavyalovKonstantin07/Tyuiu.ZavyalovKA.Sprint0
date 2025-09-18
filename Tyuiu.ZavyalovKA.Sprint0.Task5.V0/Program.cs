d45a3b3937cc6af2950b663bec983af7c192d4b3
Константин Завьялов <Tyuiu.ZavyalovKA@outlook.com>
2025-09-18 13:01:04 +0500

    Task5

---
+++ Tyuiu.ZavyalovKA.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.ZavyalovKA.Sprint0.Task5.V0.Lib;
+Console.WriteLine("A + B = " + DataService.Addition(1, 5));
+Console.WriteLine("A - B = " + DataService.Subtraction(15, 5));
+Console.WriteLine("A * B = " + DataService.Multiplication(10, 10));
+Console.WriteLine("A / B = " + DataService.Division(9, 3));
+Console.ReadKey();
